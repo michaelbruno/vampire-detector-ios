74df9a55f05c8da7108f6a02d7b527fbfd155034
Michael Bruno <michael.albert.bruno@gmail.com>
2015-08-14 13:40:33 -0400

    Initial Commit

---
+++ VampireDetector/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VampireDetector
+//
+//  Created by Michael Bruno on 8/14/15.
+//  Copyright (c) 2015 Apollonarius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VampireDetector/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VampireDetector
+//
+//  Created by Michael Bruno on 8/14/15.
+//  Copyright (c) 2015 Apollonarius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
